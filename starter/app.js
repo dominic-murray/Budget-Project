e7a36c0b8b699f0fcdc8066c90cf00107143b515
dominic-murray <dominic-murray@hotmail.co.uk>
2019-03-19 17:26:46 +0000

    Update app.js
    reseting the input field back to clear and changing the focus of the cursor back to description field instead of value.

--- starter/app.js
+++ starter/app.js
@@ -105,6 +105,21 @@ var UIController = (function() {
             // insert HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
+        
+        clearFields: function() {
+            var fields, fieldsArr;
+            
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            
+            fieldsArr = Array.prototype.slice.call(fields);
+            
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+            
+            fieldsArr[0].focus();
+        },
+        
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -145,6 +160,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         
         //3. add the item to the ui
         UICtrl.addListItem(newItem, input.type);
+        
+        // clear the fields
+        UICtrl.clearFields();
+        
         //4. calculate budget 
         
         //5. display the budget to the ui.
