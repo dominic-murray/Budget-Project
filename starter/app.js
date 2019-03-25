7acd071dddb9834bbd0d901108d7be78f8244d74
dominic-murray <dominic-murray@hotmail.co.uk>
2019-03-25 11:36:58 +0000

    adding month and year
    Adding Month and year to the available budget section

--- starter/app.js
+++ starter/app.js
@@ -155,7 +155,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercentageLabel: '.item__percentage'
+        expensesPercentageLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
     
     var formatNumber = function(num, type) {
@@ -264,6 +265,14 @@ var UIController = (function() {
             
         },
         
+        displayMonth: function() {
+          var now, months, month, year;
+            now = new Date();
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel). textContent = months[month] + ' ' + year;
+        },
         
         getDOMstrings: function() {
             return DOMstrings;
@@ -376,6 +385,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     
     return {
         init: function() {
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
