0ecb03e1af21bd62ce81ff38ffb48a6c273c330a
dominic-murray <dominic-murray@hotmail.co.uk>
2019-03-25 11:51:34 +0000

    Improving ux
    Improve UX changing colours on input fields

--- starter/app.js
+++ starter/app.js
@@ -179,6 +179,12 @@ var UIController = (function() {
             // + or - before the number and 2 decimal points comma seperating the thousands. 
         };  
     
+    var nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+    
     return {
       getInput: function() {
           return {
@@ -249,12 +255,6 @@ var UIController = (function() {
             
             var fields = document.querySelectorAll(DOMstrings.expensesPercentageLabel);
             
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-            
             nodeListForEach(fields, function(current, index) {
                 if (percentages[index] > 0) {
                     current.textContent = percentages[index] + '%';
@@ -272,6 +272,21 @@ var UIController = (function() {
             month = now.getMonth();
             year = now.getFullYear();
             document.querySelector(DOMstrings.dateLabel). textContent = months[month] + ' ' + year;
+        },
+        
+        changedType: function() {
+            
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' +
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue
+            );
+            
+            nodeListForEach(fields, function(cur) {
+               cur.classList.toggle('red-focus'); 
+            });
+            
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
         },
         
         getDOMstrings: function() {
@@ -301,6 +316,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
         
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+        
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
         
         
     };
